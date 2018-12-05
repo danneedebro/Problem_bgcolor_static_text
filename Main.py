494e4b121e25f4aa94d19fcc03b01b3725255c57
danneedebro <daniel.edebro@gmail.com>
2018-12-05 10:47:04 +0100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,18 @@
+import wx
+
+
+class Example(wx.Frame):
+    def __init__(self, parent):
+        super(Example, self).__init__(parent, title='Window', size=(300, 100))
+        self.SetBackgroundColour('Black')
+        panel = wx.Panel(self)
+
+        LblTextWithBgColor = wx.StaticText(panel, label='This text should have a red background', size=(250,20), style=wx.ALIGN_CENTER)
+        LblTextWithBgColor.SetForegroundColour('Green')
+        LblTextWithBgColor.SetBackgroundColour('Red')
+        self.Show()
+
+
+app = wx.App()
+Example(None)
+app.MainLoop()
